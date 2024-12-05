0d36aa89dcd318e09fb0dd6f18f0cbcc52c33878
mxvb <maxime.verbinnen@gmail.com>
2024-12-05 11:32:11 +0100

    Day04 setup

---
+++ src/main/java/org/example/adventofcode2024/day04/Day04.java
@@ -0,0 +1,35 @@
+package org.example.adventofcode2024.day04;
+
+import org.example.adventofcode2024.InputLoader;
+
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Day04 {
+
+
+
+    public static void main(String[] args) {
+        new Day04().start();
+    }
+
+    private void start() {
+        List<String> lines = InputLoader.getInput("day03.txt");
+        String input = String.join("", lines);
+
+        partOne(input);
+        partTwo(input);
+    }
+
+    private void partOne(String input) {
+
+    }
+
+    private void partTwo(String input) {
+
+    }
+
+
+
+}
